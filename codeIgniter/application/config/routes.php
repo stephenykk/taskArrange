51d070059e9e49a8312b19473b5f37a667edc307
stephenykk <stephenykk@163.com>
2017-06-04 00:19:29 +0800

    make the history fall back, in ci

--- codeIgniter/application/config/routes.php
+++ codeIgniter/application/config/routes.php
@@ -51,6 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $route['default_controller'] = 'welcome';
 $route['default_controller'] = 'home';
-$route['404_override'] = '';
+// $route['404_override'] = '';
+$route['404_override'] = 'home/index';
 $route['translate_uri_dashes'] = FALSE;
 $route['api/(.*)'] = '/$1';
