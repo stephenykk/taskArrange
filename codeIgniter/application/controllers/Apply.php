a3ec8f9120bf5c15b28310ad4287a8e3b999cb52
stephenykk <stephenykk@163.com>
2017-06-08 21:39:49 +0800

    forget to save the file apply.php ...

--- codeIgniter/application/controllers/Apply.php
+++ codeIgniter/application/controllers/Apply.php
@@ -26,9 +26,11 @@ class Apply extends MY_Controller {
     $this->checkLogin();
 
     $data = inputData($this);
-    if ($data['title']) {// 更新记录
+    $hasTitle = array_key_exists('title', $data);
+    $hasStatus = array_key_exists('status', $data);
+    if ($hasTitle) {// 更新记录
         $this->checkAuth('roleApplyUpdate');
-    } else if ($data['status']) {
+    } else if ($hasStatus) {
        // 没有title字段，有status字段，认为是更新状态 要求有接受任务权限
         // 'created','assigned','recieved','doing','finish','delay'
         if ($data['status'] === 'assigned') {
